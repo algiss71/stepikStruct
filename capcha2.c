359d4214b4d02cd52ff2ff5e835881f58d910e1f
sergey <gemodialez71@gmail.com>
2023-05-10 19:50:35 +0300

    add new

--- capcha2.c
+++ capcha2.c
@@ -1,39 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-char cap[]="##.#########.###########.#.####.#..#..####.##..#.#.####.##.###.#####.######.###.";
+char cap[4][20] = {
+                    "##.#########.#######",
+                    "####.#.####.#..#..##",
+                    "##.##..#.#.####.##.#",
+                    "##.#####.######.###."
+                    };
 
-void capcha(int n)
+int ndn(const long long int n)
 {
-    int nn = n;
-    size_t num = 0;
-    while (nn > 0)    {
-        nn /= 10;
-        ++num;
-    }
-    
-    int* arrNum = (int*)malloc(num*sizeof(int));
-    for (size_t i = num - 1; n > 0; --i)    {
-        arrNum[i] = n % 10;
-        n /= 10;
-    }
-        
-    int i = 0;
-    for (i = arrNum[i] * 2; i < 80; i += 20){
-        for (size_t j = 0; j < num; j++){
-            printf("%c%c", cap[j],cap[j+1]);
+    int num = 0;
+    if(n == 0)
+        num = 1;
+    else
+    {
+        long long int nn = n;
+        while (nn > 0)
+        {
+            nn /= 10;
+            ++num;
         }
-        printf("\n");    
-    }    
-        
+    }
+    return num;
+}
+int * createNumArray(const int num, long long int n)
+{
+    int* arrNum = (int*)malloc(num * sizeof(int));
     
+    if(n == 0)
+        arrNum[0] = 0;
+    else{
+        for (size_t i = num - 1; n > 0; --i){
+            arrNum[i] = n % 10;
+            n /= 10;
+        }
+
+    }
+    return arrNum;
+} 
+void capcha(int long long n)
+{
+    int num = ndn(n);
+    int* arr = NULL;        
+    arr = createNumArray(num,n);
     
+   for (int i = 0; i < 4; ++i) 
+   {
+        for(int j = 0; j < num; ++j)
+            printf("%c%c", cap[i][arr[j] * 2],cap[i][arr[j] * 2 + 1]);
+        printf("\n");        
+   }
     printf("\n");
+    
 }
 
 int main() {
 
-    int n = 0;
-    scanf("%d",&n);
+    long long int n = 0;
+    scanf("%lld",&n);
     capcha(n);
     return 0;
 }
